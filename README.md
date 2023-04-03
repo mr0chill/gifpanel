da9c2e0e2f85372750957feef2c5dcd6dd1138a4
tom-larkin <111458319+mr0chill@users.noreply.github.com>
2023-04-03 11:41:45 -0600

    New prompt based interface
    This version has a prompt that takes in an image url and optional parameters to control the look and behavior of the gif.

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # GIF Panelizer 🎞️
 
-Welcome to the fun and exciting world of GIF Panelizer! This Python script takes in PNG images, divides them into quadrants, and then turns them into super cool 4-frame GIFs. It's like magic, but with code! ✨
+Welcome to the fun and exciting world of GIF Panelizer! This Python script takes in image URLs, divides them into quadrants, and then turns them into super cool 4-frame GIFs. It's like magic, but with code! ✨
 
 ## How It Works 🛠️
 
-The script works by scanning the `IMAGES` folder for PNG files. For each image, it creates a GIF by dividing the image into four equal quadrants and using each quadrant as a frame. The processed images are then moved to the `PROCESSED` folder, while the newly created GIFs end up in the `GIFS` folder.
+The script works by taking an image URL input along with optional quality and duration parameters. It then creates a GIF by dividing the image into four equal quadrants and using each quadrant as a frame. The processed images are saved to the `PROCESSED` folder, while the newly created GIFs end up in the `GIFS` folder.
 
 Here's a quick overview of the folder structure:
 
-- `IMAGES`: Put your input PNG files here.
-- `PROCESSED`: The script moves processed PNG files to this folder.
+- `PROCESSED`: The script saves the processed image files to this folder.
 - `GIFS`: The script saves the generated GIFs in this folder.
 
 ## GIF Parameters 🎛️
@@ -24,8 +23,7 @@ The GIF is created using the following parameters:
 - `duration=100`: Sets the duration between frames in milliseconds (in this case, 100 ms or 10 fps).
 - `loop=0`: Sets the number of times the GIF loops (0 means it will loop indefinitely).
 
-These parameters can be adjusted to change the output GIF's appearance and behavior.
-
+These parameters can be adjusted by providing optional arguments while entering the image URL. Use `--q:QUALITY` and `--d:DURATION` to adjust the quality and duration, respectively.
 
 ## Example 🖼️
 
@@ -41,12 +39,30 @@ Here's an example to show you the magic in action!
 
 ## Running the Script 🏃
 
-To run the script, simply put your PNG images in the `IMAGES` folder and then execute the Python script:
+To run the script, simply execute the Python script:
 
 ```bash
 python gif_panelizer.py
 ```
 
-Voilà! 🎉 The script processes the images, moves them to the PROCESSED folder, and creates GIFs in the GIFS folder.
+The script will prompt you to enter the image URL along with optional quality and duration parameters.
+
+Example:
+
+```Enter the image URL and optional arguments: https://imageurl.com/image.png --q:95 --d:500
+```
+
+### Optional Arguments
+You can provide optional arguments to adjust the quality and duration of the generated GIF:
+
+--q:QUALITY: Sets the quality of the GIF (larger values result in better quality but larger file sizes). Acceptable range: 1 to 100. Default value: 95.
+
+--d:DURATION: Sets the duration between frames in milliseconds (shorter durations result in faster animations). Acceptable range: any positive integer value. Default value: 100.
+
+For example, if you want to create a GIF with a quality of 80 and a duration of 200 milliseconds between frames, you can enter the following:
+```Enter the image URL and optional arguments (--q:QUALITY --d:DURATION): https://imageurl.com/image.png --q:80 --d:200
+```
+
+Voilà! 🎉 The script processes the image, saves it to the PROCESSED folder, and creates a GIF in the GIFS folder with the specified quality and duration.
 
 Now go on and create some amazing GIFs! 🌟
--- gifpanel.py
+++ gifpanel.py
@@ -1,17 +1,19 @@
 import os
-import shutil
+import re
+import requests
 from PIL import Image
+from io import BytesIO
+import time
 
-def create_gif_from_quadrants(image_path, output_path):
-    img = Image.open(image_path)
-    width, height = img.size
+def create_gif_from_quadrants(image, output_path, quality, duration):
+    width, height = image.size
     frame_width, frame_height = width // 2, height // 2
-    
+
     frames = [
-        img.crop((0, 0, frame_width, frame_height)),
-        img.crop((frame_width, 0, width, frame_height)),
-        img.crop((0, frame_height, frame_width, height)),
-        img.crop((frame_width, frame_height, width, height)),
+        image.crop((0, 0, frame_width, frame_height)),
+        image.crop((frame_width, 0, width, frame_height)),
+        image.crop((0, frame_height, frame_width, height)),
+        image.crop((frame_width, frame_height, width, height)),
     ]
 
     frames[0].save(
@@ -20,32 +22,63 @@ def create_gif_from_quadrants(image_path, output_path):
         save_all=True,
         append_images=frames[1:],
         optimize=True,
-        quality=95,
-        duration=100,
+        quality=quality,
+        duration=duration,
         loop=0,
     )
 
-input_folder = "IMAGES"
-processed_folder = "PROCESSED"
-gifs_folder = "GIFS"
+def process_image():
+    processed_folder = "PROCESSED"
+    gifs_folder = "GIFS"
 
-if not os.path.exists(processed_folder):
-    os.makedirs(processed_folder)
+    if not os.path.exists(processed_folder):
+        os.makedirs(processed_folder)
 
-if not os.path.exists(gifs_folder):
-    os.makedirs(gifs_folder)
+    if not os.path.exists(gifs_folder):
+        os.makedirs(gifs_folder)
 
-for root, _, files in os.walk(input_folder):
-    for file in files:
-        if file.lower().endswith(".png"):
-            input_path = os.path.join(root, file)
-            img = Image.open(input_path)
-            
-            processed_path = os.path.join(processed_folder, file)
-            img.save(processed_path)
-            shutil.move(input_path, processed_path)
+    input_text = input("Enter the image URL and optional arguments: ")
 
-            gif_output_path = os.path.join(gifs_folder, file.rsplit(".", 1)[0] + ".gif")
-            create_gif_from_quadrants(processed_path, gif_output_path)
+    quality = 95
+    duration = 100
 
-print("Processing complete.")
+    quality_arg = re.search(r"--q:(\d+)", input_text)
+    if quality_arg:
+        quality = int(quality_arg.group(1))
+
+    duration_arg = re.search(r"--d:(\d+)", input_text)
+    if duration_arg:
+        duration = int(duration_arg.group(1))
+
+    image_url = re.sub(r"\s*--q:\d+|--d:\d+", "", input_text)
+
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
+
+    filename = os.path.basename(image_url)
+    file_extension = os.path.splitext(filename)[1]
+    processed_path = os.path.join(processed_folder, filename)
+    image.save(processed_path, format=file_extension[1:])
+
+    gif_output_path = os.path.join(gifs_folder, filename.rsplit(".", 1)[0] + ".gif")
+    create_gif_from_quadrants(image, gif_output_path, quality, duration)
+
+    print("Processing complete.")
+
+def main():
+    while True:
+        process_image()
+        timeout = 180  # 3 minutes timeout in seconds
+        print(f"Do you want to create another GIF?")
+        start_time = time.time()
+
+        while time.time() - start_time < timeout:
+            answer = input()
+            if answer.lower() in ['', 'y', 'yes']:
+                break
+            elif answer.lower() in ['n', 'no', 'nah']:
+                return
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
